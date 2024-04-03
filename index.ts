98d9ad0ba7d3155f1774dc3eb3521a92b7d15073
Samuel Webster <samuel.webster515@gmail.com>
2024-04-03 22:47:32 +0000

    lint

--- index.ts
+++ index.ts
@@ -6,39 +6,39 @@
  * @since   2024-04-03
  */
 
-import { createPrompt } from "bun-promptx";
+import { createPrompt } from "bun-promptx"
 
 function maxRunFunction(inputStr) {
   /*
    * This function calculates the max run in a string
    */
-  let maxRun = 0;
-  let currentRun = 0;
+  let maxRun = 0
+  let currentRun = 0
 
   for (let i = 0; i < inputStr.length; i++) {
     if (inputStr[i] === inputStr[i + 1]) {
-      currentRun++;
+      currentRun++
       if (currentRun > maxRun) {
-        maxRun = currentRun;
+        maxRun = currentRun
       }
     } else {
-      currentRun = 1;
+      currentRun = 1
     }
   }
 
-  return maxRun;
+  return maxRun
 }
 
 // input
-const input = createPrompt("Enter a string: ");
-const inputStr = input.value;
+const input = createPrompt("Enter a string: ")
+const inputStr = input.value
 
 // process & output
 if (inputStr.length) {
-  let maxRun = maxRunFunction(inputStr);
-  console.log(`The string "${inputStr}" has a max run of ${maxRun}!`);
+  let maxRun = maxRunFunction(inputStr)
+  console.log(`The string "${inputStr}" has a max run of ${maxRun}!`)
 } else {
-  console.log("Invalid input.");
+  console.log("Invalid input.")
 }
 
-console.log("\nDone.");
+console.log("\nDone.")
